54ef75b90a4e2b09a4d0e34f6ab89c7e75f954fe
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-11-30 11:01:19 +0100

    [add] constants

--- src/components/dashboard/components/Dashboard.js
+++ src/components/dashboard/components/Dashboard.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Card , Layout, Menu } from 'antd';
+import { Badge, Card, Layout, Menu } from 'antd';
+// import { Chart, Axis, Tooltip, Geom } from 'bizcharts';
 
-import { DASHBOARD_CONTENT } from '../constants';
+import { CLASSNAMES, STRINGS } from '../constants';
 import sideMenu from '../../sideMenu';
 
+const { DASHBOARD_CONTENT, DASHBOARD_CARD } = CLASSNAMES;
+const { APPROVED_CAMPAIGNS, LOCATIONS, PENDING_APPROVAL, STAFF } = STRINGS;
+// const data = [];
 const { SideMenu } = sideMenu.components;
 const { Content, Sider } = Layout;
 const { Item } = Menu;
@@ -13,14 +17,37 @@ const Dashboard = () => (
         <Sider>
             <SideMenu />
         </Sider>
-        <Content className={DASHBOARD_CONTENT}>
-            <Menu mode={'horizontal'}>
-                <Item><Card>Default</Card></Item>
-                <Item><Card>Default</Card></Item>
-                <Item><Card>Default</Card></Item>
-                <Item><Card>Default</Card></Item>
+        <Content>
+            <Menu className={DASHBOARD_CONTENT} mode="horizontal">
+                <Item>
+                    <Badge count={10}>
+                        <Card className={DASHBOARD_CARD}>{PENDING_APPROVAL}</Card>
+                    </Badge>
+                </Item>
+                <Item>
+                    <Badge count="99+">
+                        <Card className={DASHBOARD_CARD}>{APPROVED_CAMPAIGNS}</Card>
+                    </Badge>
+                </Item>
+                <Item>
+                    <Badge count={54}>
+                        <Card className={DASHBOARD_CARD}>{LOCATIONS}</Card>
+                    </Badge>
+                </Item>
+                <Item>
+                    <Badge count={7}>
+                        <Card className={DASHBOARD_CARD}>{STAFF}</Card>
+                    </Badge>
+                </Item>
             </Menu>
         </Content>
+        {/* <Chart height={400} data={data} forceFit>
+            <Axis name="month" />
+            <Axis name="temperature" label={{ formatter: val => `${val}°C` }} />
+            <Tooltip crosshairs={{ type: 'y' }} />
+            <Geom type="line" position="month*temperature" size={2} color="city" />
+            <Geom type="point" position="month*temperature" size={4} color="city" />
+        </Chart> */}
     </Layout>
 );
 
--- src/components/dashboard/constants.js
+++ src/components/dashboard/constants.js
@@ -1,2 +1,17 @@
 /** @constant */
-export const DASHBOARD_CONTENT = 'dashboard-content';
+export const ICONS = {
+};
+
+/** @constant */
+export const STRINGS = {
+    APPROVED_CAMPAIGNS: 'Approved Campaigns',
+    LOCATIONS: 'locations',
+    PENDING_APPROVAL: 'Pending Approval',
+    STAFF: 'Staff',
+};
+
+/** @constant */
+export const CLASSNAMES = {
+    DASHBOARD_CARD: 'dashboard-card',
+    DASHBOARD_CONTENT: 'dashboard-content',
+};
