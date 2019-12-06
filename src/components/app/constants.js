d91130449ddcebb5150dfad5a2dc48e34330b101
Simpcyclassy <chioma.onyekpere.185617@unn.edu.ng>
2019-12-06 14:46:18 +0100

    [remove] classnames from constants

--- src/components/app/constants.js
+++ src/components/app/constants.js
@@ -15,10 +15,3 @@ export const VERTICAL = 'vertical';
 
 /** @constant */
 export const FOOTER_TEXT = 'NEONATAR ©2018, TEAM HIPPO';
-
-/** @constant */
-export const CLASSNAMES = {
-    HEADER_MENU: 'header-menu',
-    LEFT_NAV_MENU: 'left-nav-menu',
-    RIGHT_NAV_MENU: 'right-nav-menu',
-};
