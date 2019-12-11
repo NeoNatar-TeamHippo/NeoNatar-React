c3007a44864820d7b7555f08e792751ee82c9d73
Okafor Jude <Okaforjudechukwuebuka@gmail.com>
2019-12-11 15:37:35 +0100

    [fix] implement feedback

--- src/components/locations/components/Locations.js
+++ src/components/locations/components/Locations.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import { Typography, Button, Row, Col, Input, Icon } from 'antd';
+import { ADD_SELECTED, OUR_LOCATION, SORT, FILTER } from '../constants';
+
 const { Text, Title } = Typography;
-const Locations = () => {
-    return (
-        <div className='container-fluid'>
-            <Row >
-                <Col span={8}>
-                    <Title level={4}>
-                        Our Locations
-                            <Button className='ml-2' type='primary'>
-                            Add Selected
-                        </Button>
-                    </Title>
-                </Col>
-                <Col span={8}>
-                    <Input
-                        prefix={<Icon type="search" />}
-                        placeholder="Search..."
-                    />
-                </Col>
-                <Col span={8} >
-                    <Text type='warning'>Sort</Text> |
-                        <Text type='danger'>Filter</Text>
-                </Col>
-            </Row>
-        </div>
-    )
-}
+const Locations = () => (
+    <div className="container-fluid">
+        <Row>
+            <Col span={8}>
+                <Title level={4}>
+                    {OUR_LOCATION}
+                    <Button className="ml-2" type="primary">
+                        {ADD_SELECTED}
+                    </Button>
+                </Title>
+            </Col>
+            <Col span={8}>
+                <Input
+                    prefix={<Icon type="search" />}
+                    placeholder="Search..."
+                />
+            </Col>
+            <Col span={8}>
+                <Text type="warning">{SORT}</Text>
+                <Text type="danger">{FILTER}</Text>
+            </Col>
+        </Row>
+    </div>
+);
 export default Locations;
