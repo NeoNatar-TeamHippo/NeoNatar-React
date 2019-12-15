0b9e28829244b1b083733ed102234ca6970456ad
Okafor Jude <Okaforjudechukwuebuka@gmail.com>
2019-12-15 17:28:33 +0100

    [fix] location component

--- src/components/locations/components/Locations.js
+++ src/components/locations/components/Locations.js
@@ -1,34 +1,21 @@
 import React from 'react';
-import { Typography, Button, Row, Col, Input } from 'antd';
+import { Typography, Button, Row, Col } from 'antd';
 import LocationTable from './Table';
-import { ADD_SELECTED, OUR_LOCATION, SORT, FILTER } from '../constants';
+import { ADD_SELECTED, OUR_LOCATION } from '../constants';
 
-const { Text, Title } = Typography;
+const { Title } = Typography;
 const Locations = () => (
     <div className="container">
         <Row>
-            <Col span={8}>
-                <div className="d-flex justify-content-start">
-                    <Title level={4}>
-                        {OUR_LOCATION}
-                        <Button className="ml-3" size="default" type="primary">
-                            {ADD_SELECTED}
-                        </Button>
-                    </Title>
-                </div>
+            <Col sm={8} md={6} lg={4}>
+                <Title level={4}>
+                    {OUR_LOCATION}
+                </Title>
             </Col>
-            <Col span={10}>
-                {/* <div className="d-flex justify-content-center mt-1">
-                    <Input.Search
-                        placeholder="Search..."
-                    />
-                </div> */}
-            </Col>
-            <Col span={6}>
-                <div className="d-flex justify-content-end mt-2">
-                    <Text type="warning">{SORT}</Text>
-                    <Text type="danger">{FILTER}</Text>
-                </div>
+            <Col sm={16} md={18} lg={20}>
+                <Button className="mb-2" size="default" type="primary">
+                    {ADD_SELECTED}
+                </Button>
             </Col>
         </Row>
         <LocationTable />
