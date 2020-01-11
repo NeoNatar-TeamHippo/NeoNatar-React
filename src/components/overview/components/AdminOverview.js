e79fd224f7ca1910a2edf72667cda80928c3fc7b
Simpcyclassy <chioma.onyekpere@gmail.com>
2020-01-11 20:30:50 +0100

    [fix] make card responsive

--- src/components/overview/components/AdminOverview.js
+++ src/components/overview/components/AdminOverview.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Col, Row, Typography, Tag } from 'antd';
+import { NavLink } from 'react-router-dom';
 
 import UnresolvedTickets from './UnresolvedTickets';
 import Tasks from './Tasks';
@@ -11,46 +12,29 @@ const { Text } = Typography;
 const Dashboard = () => (
     <div className="dashboard-div">
         <Row gutter={48}>
-            {ADMIN_CARDS.map(({ color, counts, type }) => (
-                <Col key={type} span={6}>
-                    <Card className="dashboard-card">
-                        <Text className="notification-card-text">{type}</Text>
-                        <br />
-                        <Tag class="notification-card-tag" color={color} key={type}>
-                            {counts}
-                        </Tag>
-                    </Card>
+            {ADMIN_CARDS.map(({ color, counts, type, link }) => (
+                <Col className="dashboard-card-col" key={type} span={6}>
+                    <NavLink to={link}>
+                        <Card hoverable className="dashboard-card">
+                            <Text className="notification-card-text">{type}</Text>
+                            <br />
+                            <Tag class="notification-card-tag" color={color} key={type}>
+                                {counts}
+                            </Tag>
+                        </Card>
+                    </NavLink>
                 </Col>
             ))}
         </Row>
         <Row gutter={48}>
-            <Col key="unresolved-ticket" span={11}>
+            <Col key="unresolved-ticket" span={12}>
                 <UnresolvedTickets className="dashboard-ticket" />
             </Col>
-            <Col key="tasks" span={11}>
+            <Col key="tasks" span={12}>
                 <Tasks className="dashboard-ticket" />
             </Col>
         </Row>
     </div>
-    // <div style={{ padding: '30px' }}>
-    //     <Row gutter={16}>
-    //         <Col span={8}>
-    //             <Card bordered>
-    //                 Card content
-    //             </Card>
-    //         </Col>
-    //         <Col span={8}>
-    //             <Card bordered>
-    //                 Card content
-    //             </Card>
-    //         </Col>
-    //         <Col span={8}>
-    //             <Card bordered>
-    //                 Card content
-    //             </Card>
-    //         </Col>
-    //     </Row>
-    // </div>
 );
 
 export default Dashboard;
