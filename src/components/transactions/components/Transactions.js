46adba2d8d63aeede719e9248cd9fe999d371086
Simpcyclassy <chioma.onyekpere@gmail.com>
2020-01-17 16:39:51 +0100

    [remove] menu tab

--- src/components/transactions/components/Transactions.js
+++ src/components/transactions/components/Transactions.js
@@ -1,43 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Button, PageHeader, Tag, Table, Row, Col, Menu, Tooltip } from 'antd';
+import { Button, PageHeader, Table, Tooltip } from 'antd';
 
-import { ALL, VALID, INVALID } from '../constants';
 import { requestTransactions } from '../actions';
-
-const menuItems = [ALL, VALID, INVALID];
 
 const Transactions = ({ history }) => {
     const { transactions, isTransactionsLoading } = useSelector(state => state.transactions);
-
-    const [transactionsData, setTransactionsData] = useState(transactions);
 
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(requestTransactions());
     }, [dispatch]);
-
-    const handleChangeTab = ({ key }) => {
-        switch (key) {
-            case ALL:
-                setTransactionsData(transactions);
-                break;
-            case VALID:
-                setTransactionsData(transactions.filter(
-                    transaction => transaction.status === 'valid'
-                ));
-                break;
-            case INVALID:
-                setTransactionsData(transactions.filter(
-                    transaction => transaction.status === 'invalid'
-                ));
-                break;
-            default:
-                setTransactionsData(transactions);
-                break;
-        }
-    };
 
     const columns = [
         {
@@ -59,24 +33,6 @@ const Transactions = ({ history }) => {
             dataIndex: 'createdAt',
             key: 'createdAt',
             title: 'Date',
-        },
-        {
-            dataIndex: 'status',
-            key: 'status',
-            render: status => {
-                let color;
-                if (status === 'invalid') {
-                    color = 'red';
-                } else {
-                    color = 'green';
-                }
-                return (
-                    <Tag color={color} key={status}>
-                        {status.toUpperCase()}
-                    </Tag>
-                );
-            },
-            title: 'Status',
         },
         {
             key: 'action',
@@ -101,23 +57,10 @@ const Transactions = ({ history }) => {
                 title="Transactions"
                 className="mb-2 page_header"
             />
-            <Row type="flex" style={{ marginBottom: 5 }}>
-                <Col span={14}>
-                    <Menu mode="horizontal" onClick={handleChangeTab}>
-                        {
-                            menuItems.map(key => (
-                                <Menu.Item key={key}>
-                                    {key}
-                                </Menu.Item>
-                            ))
-                        }
-                    </Menu>
-                </Col>
-            </Row>
             <Table
                 loading={isTransactionsLoading}
                 columns={columns}
-                dataSource={transactionsData}
+                dataSource={transactions}
                 rowKey={record => record.id}
             />
         </div>
