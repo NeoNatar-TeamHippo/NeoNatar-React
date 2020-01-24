6e7b5c800df9b6c5fff85243425ee6f4ed336a3f
Simpcyclassy <chioma.onyekpere@gmail.com>
2020-01-24 10:59:44 +0100

    [add] row col to home page

--- src/components/home/components/Home.js
+++ src/components/home/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+
 import layouts from '../../layouts';
 import art from '../../../images/illustration.png';
 import text from '../../../images/text.png';
@@ -7,13 +9,15 @@ const { HomeLayout } = layouts.components;
 
 const Home = () => (
     <HomeLayout>
-        <div className="home-page">
-            <div className="image-container">
-                <img src={text} alt="dummy-text" />
-            </div>
-            <div className="image-container hide-image-container">
-                <img src={art} alt="illustration" />
-            </div>
+        <div>
+            <Row>
+                <Col span={12}>
+                    <img src={text} alt="dummy-text" />
+                </Col>
+                <Col className="image-container hide-image-container" span={12}>
+                    <img src={art} alt="illustration" />
+                </Col>
+            </Row>
         </div>
     </HomeLayout>
 );
