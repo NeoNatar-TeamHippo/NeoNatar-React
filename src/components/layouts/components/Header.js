39221b5cebee721bb4cd3794331183db545b8791
Simpcyclassy <chioma.onyekpere@gmail.com>
2020-01-25 21:10:58 +0100

    [add] user icon

--- src/components/layouts/components/Header.js
+++ src/components/layouts/components/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Button, Menu, Layout } from 'antd';
+import { Button, Menu, Layout, Avatar } from 'antd';
+import UserLogo from '../../../images/user.svg';
 
-import Logo from '../../../images/mock-logo.png';
+import Logo from '../../../images/neoNatar Logo.svg';
 
-import { SIGNIN, SIGNUP, GO_TO_DASHBOARD } from '../constants';
+import { SIGNIN, SIGNUP } from '../constants';
 
 const { Item } = Menu;
 const { Header } = Layout;
 
 const NavHeader = () => {
+    const { user, navLoading } = useSelector(state => state.user);
+
     const { location } = useSelector(state => state.router);
     const { authenticated } = useSelector(state => state.signIn);
 
@@ -23,6 +26,14 @@ const NavHeader = () => {
             </Button>
         </Item>
     );
+    const userIcon = () => (
+        <Item className="header-item">
+            <span>
+                {navLoading ? (<Avatar src={UserLogo} />)
+                    : (<Avatar src={user.avatar} />)}
+            </span>
+        </Item>
+    );
 
     const renderMenuItem = ({ pathname }) => {
         if (pathname === '/signup') {
@@ -32,7 +43,7 @@ const NavHeader = () => {
             return menuItem('/signup', SIGNUP, true);
         }
         if (authenticated) {
-            return menuItem('/dashboard', GO_TO_DASHBOARD, false);
+            return userIcon();
         }
         return [
             menuItem('/signin', SIGNIN, false),
