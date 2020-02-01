7223a16ecd155f57bdbaf6c868d08766b6eec917
Simpcyclassy <chioma.onyekpere@gmail.com>
2020-02-01 18:12:40 +0100

    [add] testimonials

--- src/components/home/constants.js
+++ src/components/home/constants.js
@@ -16,6 +16,8 @@ export const LANDING_TITLE = 'Put your content and ads in front of Restaurant tr
 /** @constant */
 export const EXPLAINERS_TITLE = 'Get your business out';
 /** @constant */
+export const TESTIMONIALS = 'TESTIMONIALS';
+/** @constant */
 export const HALF_LANDING_TITLE = 'Why choose us?';
 /** @constant */
 export const LANDING_DESCRIPTION = 'We are expanding into secure indoor locations with very high traffic or gathering, such as: restaurants, supermarkets, covered pavilions, football houses.';
@@ -82,5 +84,39 @@ export const EXPLAINER_ITEMS = [
     {
         description: 'You can advertise for as low as 1 location for 1 day, if that is what you can afford.',
         key: 4,
+    },
+];
+
+/** @constant */
+export const TESTIMONIAL_ITEMS = [
+    {
+        company: 'Heritage Bank',
+        name: 'Fisayo Olaleye',
+        testimonial: 'It is a good way for brands to use content as an attraction, on one channel, to show ads to its customers on another channel.',
+    },
+    {
+        company: 'Peak Tutors',
+        name: 'Matthew IGBA',
+        testimonial: 'We are using this platform as an Avenue to show tutorial videos, to our target audience in restaurants they visit.',
+    },
+    {
+        company: 'ePower',
+        name: 'Ogunbowale Olugbenga',
+        testimonial: 'Streaming content and ads side by side, is definitely a thing to work with, for anyone that understands what content does to ads.',
+    },
+    {
+        company: 'PipulTV',
+        name: 'Benjamin Okorafor',
+        testimonial: 'This is so much value for money. It is a combination of technology, reach, engagements, convenience, flexibility and affordability.',
+    },
+    {
+        company: 'AceRide',
+        name: 'Johnson Alabi',
+        testimonial: 'As everything is rapidly going online, this is one of the best affordable reasons to retain offline in our company\'s budget.',
+    },
+    {
+        company: 'Dove',
+        name: 'Joshua Ale',
+        testimonial: 'We had plans to do gadget videos but traditional TV is too expensive for us. We chose this because we can target the restaurants our target audience visit.',
     },
 ];
